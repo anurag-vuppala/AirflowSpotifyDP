1c6e454682fbd83330b9364e12a2dff4c3e32cec
anurag-vuppala <75685045+anurag-vuppala@users.noreply.github.com>
2022-02-21 19:44:14 +0000

    working sheet api
    adding values from sqlite to sheet

--- google_sheet_api.py
+++ google_sheet_api.py
@@ -1,79 +1,84 @@
-# from pydoc import cli
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-# # import google.oauth2.service_account as gs
-# from pprint import pprint
-
-# scope = ['https://www.googleapis.com/auth/drive',
-#            'https://www.googleapis.com/auth/drive.file',
-#            'https://www.googleapis.com/auth/drive.readonly',
-#            'https://www.googleapis.com/auth/spreadsheets',
-#            'https://www.googleapis.com/auth/spreadsheets.readonly',
-#            ]
-
-# creds = ServiceAccountCredentials.from_json_keyfile_name('sheet_creds.json',scope)
-
-# client = gspread.authorize(creds) 
-
-# sheet = client.open("api").sheet1
-
-# data = sheet.get_all_records()
-
-# insertRow = ["2","anurag","vuppala"]
-
-# sheet.insert_rows(insertRow, 2)
-
-# pprint(data)
-
-
-
-
 from curses import raw
+import sqlite3
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import json
-
+import sqlalchemy
 from numpy import insert
 
 
-key = 'sheet_creds.json'
+def gett_data_from_database():
+    DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cur = conn.cursor()
+    data = cur.execute("select * from my_played_tracks").fetchall()
+    return data
 
-scope = ['https://www.googleapis.com/auth/drive',
-         'https://www.googleapis.com/auth/drive.file',
-         'https://www.googleapis.com/auth/drive.readonly',
-         'https://www.googleapis.com/auth/spreadsheets',
-         'https://www.googleapis.com/auth/spreadsheets.readonly',
-            ]
+def append_data_to_sheet(data,sheet_name):
+    key = 'sheet_creds.json'
+    scope = ['https://www.googleapis.com/auth/drive',
+            'https://www.googleapis.com/auth/drive.file',
+            'https://www.googleapis.com/auth/drive.readonly',
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/spreadsheets.readonly',
+                ]
 
-creds = None
-creds = service_account.Credentials.from_service_account_file(key,scopes=scope)
+    creds = None
+    creds = service_account.Credentials.from_service_account_file(key,scopes=scope)
+    sheetid = '1bdPEicAieaus4AWU92_Ry11f4XMiYJSncJiWh42bd28'
+    service = build('sheets','v4',credentials=creds)
+    sheet = service.spreadsheets()
+    ranges = sheet_name + '!A1' 
+    apnd = sheet.values().append(spreadsheetId=sheetid, range=ranges,valueInputOption='USER_ENTERED', insertDataOption='INSERT_ROWS', body = {"values": data}).execute()
+    return apnd
 
-sheetid = '1bdPEicAieaus4AWU92_Ry11f4XMiYJSncJiWh42bd28'
 
-service = build('sheets','v4',credentials=creds)
+data = gett_data_from_database()
+sheet_name = "spotify"
+add = append_data_to_sheet(data,sheet_name)
+print(add)
 
-sheet = service.spreadsheets()
+
+# key = 'sheet_creds.json'
+# scope = ['https://www.googleapis.com/auth/drive',
+#          'https://www.googleapis.com/auth/drive.file',
+#          'https://www.googleapis.com/auth/drive.readonly',
+#          'https://www.googleapis.com/auth/spreadsheets',
+#          'https://www.googleapis.com/auth/spreadsheets.readonly',
+#             ]
+
+# creds = None
+# creds = service_account.Credentials.from_service_account_file(key,scopes=scope)
+# sheetid = '1bdPEicAieaus4AWU92_Ry11f4XMiYJSncJiWh42bd28'
+# service = build('sheets','v4',credentials=creds)
+# sheet = service.spreadsheets()
 # this is to get the values from the sheet
-result = sheet.values().get(spreadsheetId=sheetid, range="sheet1!A1:C5").execute()
+# result = sheet.values().get(spreadsheetId=sheetid, range="sheet1!A1:C5").execute()
 
 
-# this is to update values in the sheet
+# # this is to update values in the sheet
 
-input_value = [[1,"anurag", "vuppala"],[2,"anushike","biskit"]]
+# # input_value = [[1,"anurag", "vuppala"],[2,"anushike","biskit"]]
 
-result = sheet.values().update(spreadsheetId=sheetid, range="sheet1!A1", valueInputOption='USER_ENTERED', body = {"values": input_value}).execute()
-print(result)
+# # result = sheet.values().update(spreadsheetId=sheetid, range="sheet1!A1", valueInputOption='USER_ENTERED', body = {"values": input_value}).execute()
+# print(result)
 
 ########################
 
-#to append values to sheet
-append_data = [[3,"venkataswaramma","vuppla"]]
+#to append values to sheet , will add data at the end of the list
 
-apnd = sheet.values().append(spreadsheetId=sheetid, range="sheet1!A1",valueInputOption='USER_ENTERED', insertDataOption='INSERT_ROWS', body = {"values": append_data}).execute()
+# append_data = [[3,"venkataswaramma","vuppla"]]
 
 
-print(result)
+
+
 #####################
+
+
+
+print(gett_data_from_database())
+    
 
 
 
--- try.py
+++ try.py
@@ -8,81 +8,88 @@ from datetime import datetime, timedelta
 
 import sqlite3
 
-DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID =  	"31c46elir35wdgopjjfnkfilf4ba"       # your Spotify username 
-TOKEN = "BQCPXjcjdDSNISFHFQxZFG16bCLbcczzNowgabQsaFVeK6sNdd2PNE8ZHsBcQzV4z8w29olTW0uAv156deNjq_DSO3yF-d_cZ7MvjPG1FnJyM3Fec7XoFlkrPVAeB6AHA7O5bhkP1sEKkocZ6HUSrNwhcGZm8JVsgMfnfpehpT_CyWjrPVR9tGvl04ZwnQ" # your Spotify API token
+# DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+# USER_ID =  	"31c46elir35wdgopjjfnkfilf4ba"       # your Spotify username 
+# TOKEN = "BQCPXjcjdDSNISFHFQxZFG16bCLbcczzNowgabQsaFVeK6sNdd2PNE8ZHsBcQzV4z8w29olTW0uAv156deNjq_DSO3yF-d_cZ7MvjPG1FnJyM3Fec7XoFlkrPVAeB6AHA7O5bhkP1sEKkocZ6HUSrNwhcGZm8JVsgMfnfpehpT_CyWjrPVR9tGvl04ZwnQ" # your Spotify API token
  
-headers = {
-    "Accept" : "application/json",
-    "Content-Type" : "application/json",
-    "Authorization" : "Bearer {token}".format(token=TOKEN)
-    }
+# headers = {
+#     "Accept" : "application/json",
+#     "Content-Type" : "application/json",
+#     "Authorization" : "Bearer {token}".format(token=TOKEN)
+#     }
     
-    # Convert time to Unix timestamp in miliseconds      
-today = datetime.now()
-yesterday = today - timedelta(days=1)
-yesterday_unix_timestamp = int(today.timestamp()) * 1000
+#     # Convert time to Unix timestamp in miliseconds      
+# today = datetime.now()
+# yesterday = today - timedelta(days=1)
+# yesterday_unix_timestamp = int(today.timestamp()) * 1000
 
-# Download all songs you've listened to "after yesterday", which means in the last 24 hours   
-r = requests.get("https://api.spotify.com/v1/me/tracks?market=us", headers=headers)
-
-
-data = r.json()
+# # Download all songs you've listened to "after yesterday", which means in the last 24 hours   
+# r = requests.get("https://api.spotify.com/v1/me/tracks?market=us", headers=headers)
 
 
-song_names = []
-artist_names = []
-played_at_list = []
+# data = r.json()
 
 
-# Extracting only the relevant bits of data from the json object      
-for song in data["items"]:
-    print(song)
-    song_names.append(song["track"]["name"])
-    artist_names.append(song["track"]["album"]["artists"][0]["name"])
-    played_at_list.append(song["added_at"])
+# song_names = []
+# artist_names = []
+# played_at_list = []
+
+
+# # Extracting only the relevant bits of data from the json object      
+# for song in data["items"]:
+#     print(song)
+#     song_names.append(song["track"]["name"])
+#     artist_names.append(song["track"]["album"]["artists"][0]["name"])
+#     played_at_list.append(song["added_at"])
     
 
-# Prepare a dictionary in order to turn it into a pandas dataframe below       
-song_dict = {
-    "song_name" : song_names,
-    "artist_name": artist_names,
-    "added_at" : played_at_list,
+# # Prepare a dictionary in order to turn it into a pandas dataframe below       
+# song_dict = {
+#     "song_name" : song_names,
+#     "artist_name": artist_names,
+#     "added_at" : played_at_list,
     
-}
+# }
 
-song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "added_at"])
-
-
-print(song_df)
+# song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "added_at"])
 
 
-#  # Validate
-#   if check_if_valid_data(song_df):
-#      print("Data valid, proceed to Load stage")
+# print(song_df)
 
-# Load
 
-# engine = sqlalchemy.create_engine(DATABASE_LOCATION)
-# conn = sqlite3.connect('my_played_tracks.sqlite')
-# cursor = conn.cursor()
+# #  # Validate
+# #   if check_if_valid_data(song_df):
+# #      print("Data valid, proceed to Load stage")
 
-# sql_query = """
-# CREATE TABLE IF NOT EXISTS my_played_tracks(
-# song_name VARCHAR(200),
-# artist_name VARCHAR(200),
-# added_at VARCHAR(200),
-# CONSTRAINT primary_key_constraint PRIMARY KEY (aded_at)
-# )
-# """
+# # Load
 
-# cursor.execute(sql_query)
-# print("Opened database successfully")
+# # engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+# # conn = sqlite3.connect('my_played_tracks.sqlite')
+# # cursor = conn.cursor()
 
-# try:
-#     song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
-# except:
-#     print("Data already exists in the database")
+# # sql_query = """
+# # CREATE TABLE IF NOT EXISTS my_played_tracks(
+# # song_name VARCHAR(200),
+# # artist_name VARCHAR(200),
+# # added_at VARCHAR(200),
+# # CONSTRAINT primary_key_constraint PRIMARY KEY (aded_at)
+# # )
+# # """
 
-# conn.close()
-# print("Close database successfully")
+# # cursor.execute(sql_query)
+# # print("Opened database successfully")
+
+# # try:
+# #     song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+# # except:
+# #     print("Data already exists in the database")
+
+# # conn.close()
+# # print("Close database successfully")
+
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+conn = sqlite3.connect('my_played_tracks.sqlite')
+cur = conn.cursor()
+data = cur.execute("select * from my_played_tracks").fetchall()
+print(data.help())
